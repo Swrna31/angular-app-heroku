a141cd36fdbd233192dbcbe141dc23b5c8b9e915
Alagu Swrnam Karruppiah <al581093@dal.ca>
2022-05-25 11:47:35 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'news',component: NewsComponent},
+      {path:'about',component:AboutComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

015197c50d58cd4273d904b4f330e88d084704cd
Alagu Swrnam Karruppiah <al581093@dal.ca>
2022-05-25 12:08:12 -0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/Router-Test'));
+app.use(express.static(__dirname + '/dist/tutorial'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<app-name>/index.html'));});
+    '/dist/tutorial/index.html'));});
 app.listen(process.env.PORT || 8080);
